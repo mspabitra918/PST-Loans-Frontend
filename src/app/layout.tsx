5815822ed3b98707e62acbeb6b53451f036fb611
Pabitra Ghara <pabitraghara384@gmail.com>
2026-04-13 15:45:26 +0530

    Refactor : Pixel is not set up on homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,8 +45,33 @@ export default function RootLayout({
             gtag('config', 'G-M8HFKQRMST');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2392306191230857');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2392306191230857&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <Toaster position="top-right" />
       </body>
